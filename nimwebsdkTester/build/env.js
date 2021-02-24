78500538ad26ffcdf76d22f5b148fb02add4078a
scaret <scaret.in@gmail.com>
2021-02-24 19:01:16 +0800

    删除了更多用不到的文件

--- nimwebsdkTester/build/env.js
+++
@@ -1,8 +0,0 @@
-
-var env = {}
-
-env.isProduction = function () {
-  return process.env.NODE_ENV === 'production'
-}
-
-module.exports = env
--- nimwebsdkTester/build/favicon.js
+++
@@ -1,7 +0,0 @@
-
-var favicons = require('favicons')
-var path = require('path')
-favicons({
-  source: path.resolve('../res/img/logo.png'),
-  dest: path.resolve('../dist/')
-})
