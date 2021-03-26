84526be11883eadb40eb45367264575e6ea6e33a
scaret <scaret.in@gmail.com>
2021-03-26 11:09:33 +0800

    修改pack:head:g2，使用build号而不是commitid做索引

--- build/git.js
+++ build/git.js
@@ -1,3 +1,4 @@
+#!/usr/local/bin/node
 const execSync = require('child_process').execSync
 const baseOpt = {
   encoding: 'utf8'
@@ -18,16 +19,15 @@ obj.getFirstCommitHash = function () {
   }
 }
 
-obj.describe = function (tagName) {
+obj.describe = function () {
   let result;
   let cmd;
-  if (tagName) {
-    try {
-      cmd = `git describe --match "*${tagName}*" --tags --long --dirty`;
-      result = execSync(cmd, baseOpt).trim();
-    } catch (e) {
-      result = execSync("git describe --tags --long --dirty", baseOpt).trim();
-    }
+  try {
+    const webrtcG2Version = require("../package.json").webrtcG2Version;
+    cmd = `git describe --match "*${webrtcG2Version}*" --tags --long --dirty`;
+    result = execSync(cmd, baseOpt).trim();
+  } catch (e) {
+    result = execSync("git describe --tags --long --dirty", baseOpt).trim();
   }
   return result;
 }
@@ -44,3 +44,11 @@ obj.currentCommit = function () {
 }
 
 module.exports = obj
+
+if (require.main === module) {
+  const cmd = process.argv[process.argv.length - 1];
+  if (cmd === "describe") {
+    const result = obj.describe();
+    process.stdout.write(result);
+  }
+}
