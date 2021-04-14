b1eec8ae6a7428c978db3489d77c6252639f84e5
scaret <scaret.in@gmail.com>
2021-04-14 10:41:13 +0800

    优化文档生成报错

--- build/api/index.js
+++ build/api/index.js
@@ -1,8 +1,21 @@
+const fs = require('fs');
+const path = require('path');
 const child_process = require('child_process')
 const pjson = require('../../package.json');
 
 const title = `NeRTC G2 v${pjson.webrtcG2Version}`
+if (fs.existsSync(path.join(__dirname, '../../doc'))){
+  console.error('文档已迁移至docs/src, 请把文件迁移到d.ts');
+  process.exit(0)
+}
 
-const status = child_process.execSync(`typedoc --name "${title}" --tsconfig ../../tsconfig_typedoc.json`)
+try {
+  const status = child_process.execSync(`typedoc --name "${title}" --tsconfig ../../tsconfig_typedoc.json`)
+} catch (e) {
+  console.error("文档生成失败");
+  console.log(e.stdout.toString('utf8'));
+  console.error(e.stderr.toString('utf8'));
+  process.exit(0);
+}
 
 // const status = child_process.execSync('jsdoc -c ./build/api/jsdoc.config.json -d ./dist/api')
