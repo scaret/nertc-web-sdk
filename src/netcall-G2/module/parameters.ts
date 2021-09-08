6481dcca96872e40519de1b23ce9ce71f8bc9e72
scaret <scaret.in@gmail.com>
2021-09-08 20:21:39 +0800

    遇到自动播放时展示默认控件；恢复后隐藏默认控件

--- src/netcall-G2/module/parameters.ts
+++ src/netcall-G2/module/parameters.ts
@@ -4,10 +4,23 @@ import {Client} from "../api/client";
 import {Stream} from "../api/stream";
 
 interface IParameters{
+  
+  // 储存了通过createClient创建的客户端
   clients: Client[];
+  
+  // 存储了通过createStream创建的客户端
   localStreams: Stream[],
+  
+  // 主流开开启小流时的视频采集参数
   videoLowDefaultConstraints: MediaTrackConstraints,
+  // 辅流开开启小流时的视频采集参数
   screenLowDefaultConstraints: MediaTrackConstraints,
+  
+  // 播放时如果遇到自动播放问题，是否显示video控件的默认控制选项
+  controlOnPaused: boolean,
+
+  // 恢复播放时，是否隐藏video控件的默认控制选项
+  hideControlOnResume: boolean,
 }
 
 let parameters:IParameters = {
@@ -15,6 +28,8 @@ let parameters:IParameters = {
   localStreams: [],
   videoLowDefaultConstraints: {width: 160},
   screenLowDefaultConstraints: {width: 160},
+  controlOnPaused: true,
+  hideControlOnResume: true,
 }
 
 // 注意：getParameters和setParameters是一些私有全局变量，仅用于调试和私有接口，不用于正常业务
