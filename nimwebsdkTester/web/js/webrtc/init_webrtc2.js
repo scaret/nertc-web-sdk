5cbad8bd31c10fa485e611e123bb2b7eb031c9c0
scaret <scaret.in@gmail.com>
2021-10-12 16:57:29 +0800

    修复自定义音视频的销毁问题

--- nimwebsdkTester/web/js/webrtc/init_webrtc2.js
+++ nimwebsdkTester/web/js/webrtc/init_webrtc2.js
@@ -838,17 +838,26 @@ $('#joinChannel-btn').on('click', async () => {
       } else if ($('#privateVideo').prop('checked')) {
         video = true
       }
-      if (audio || video) {
-        navigator.mediaDevices.getUserMedia(
-          {audio, video}
-        ).then(mediaStream => {
-          rtc.videoSource = mediaStream.getVideoTracks().length && mediaStream.getVideoTracks()[0];
-          rtc.audioSource = mediaStream.getAudioTracks().length && mediaStream.getAudioTracks()[0];
-          initLocalStream(rtc.audioSource, rtc.videoSource)
-        })
-      } else {
-        initLocalStream()
+      
+      if (audio){
+        rtc.audioSource = fakeMediaDevices.getFakeMedia({audio: true}).getTracks()[0];
+      }else{
+        if (rtc.audioSource){
+          rtc.audioSource.stop()
+          rtc.audioSource = null
+        }
       }
+      
+      if (video){
+        rtc.videoSource = fakeMediaDevices.getFakeMedia({video: {width: 1024, height: 768}, content: "自定义主流"}).getTracks()[0];
+      }else{
+        if (rtc.videoSource){
+          rtc.videoSource.stop()
+          rtc.videoSource = null
+        }
+      }
+
+      initLocalStream(rtc.audioSource, rtc.videoSource)
     }else{
       addLog("加入频道后未执行初始化本地流")
     }
@@ -887,10 +896,10 @@ $('#leaveChannel-btn').on('click', () => {
   rtc.succTasksList = []
   rtc.failTasksList = []
   if (rtc.audioSource) {
-    rtc.audioSource.map(track=>{track.stop()})
+    rtc.audioSource.stop()
   } 
   if (rtc.videoSource) {
-    rtc.videoSource.map(track=>{track.stop()})
+    rtc.videoSource.stop()
   }
   watermarks = {local: null, remote: {}};
 })
