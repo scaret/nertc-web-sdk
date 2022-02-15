5bfe2332229987aa1b28b06e135dc0102bf7207c
Derek <dereklu@corp.netease.com>
2022-02-15 08:27:55 +0000

    草莓聊无状态sdp推拉流三期

--- nimwebsdkTester/web/js/stateless/server/stateless-test-server.js
+++ nimwebsdkTester/web/js/stateless/server/stateless-test-server.js
@@ -4,6 +4,12 @@ const path = require("path")
 const request = require("request")
 const cors = require("cors");
 const bodyParser = require('body-parser')
+const fs = require("fs")
+
+const credentials = {
+  key: fs.readFileSync('/home/yunxin/.badcert/127.0.0.1/key.pem'),
+  cert: fs.readFileSync('/home/yunxin/.badcert/127.0.0.1/cert.pem'),
+}
 
 console.log(wrtc)
 
@@ -23,105 +29,140 @@ const pullers = []
 
 app.use(express.static(path.join(__dirname, "../../..")))
 
-app.post('/v1/edge/server/cid/:cid/push/request', async (req, res) => {
-  const cid = req.params.cid
-  const uid = req.body.uid
-  const offerSdp = req.body.sdp
-  console.log("PUSH ", cid, uid)
-  let pusher = pushers.find((p)=>{
-    return p.cid === cid && p.uid === uid
+app.post('/v1/rtc/publish', async (req, res) => {
+  const streamName = req.body.stream_name;
+  const update = req.body.update;
+  const jsep = req.body.jsep;
+    
+  console.log("PUBLISH", streamName, update ? "UPDATE": "NEW")
+  
+  let pusherIndex = pushers.findIndex((p)=>{
+    return p.streamName === streamName
   })
-  if (pusher){
+  let pusher = pushers[pusherIndex]
+  if (pusher && !update){
+    console.log("Close former Peerconnection", pusher.id)
+    //不是更新，把之前的流给删掉
     pusher.peerConnection.close()
-  }else{
-    pusher = {cid, uid, id: ++pusherIdx}
+    pushers.splice(pusherIndex, 1)
+    pusher = null
+  }
+
+  let candidatePromise = null
+  let pc = null
+  if (!pusher){
+    pc = new wrtc.RTCPeerConnection();
+    pusher = {
+      streamName,
+      id: ++pusherIdx,
+      peerConnection: pc,
+      tracks: []
+    }
+    pc.onsignalingstatechange = (evt)=>{
+      const tracksStatus = pusher.tracks.map((trackInfo)=>{
+        return trackInfo.track.kind + " " + trackInfo.track.readyState
+      }).join()
+      console.log(`PUSHER#${pusher.id} onsignalingstatechange ${pc.signalingState}. Tracks ${tracksStatus}`)
+    }
+    pc.ontrack = (evt)=>{
+      const id = ++trackIdx
+      console.log(`#${pusher.id} ontrack ${id}`, evt.track.kind)
+      pusher.tracks.unshift({
+        id,
+        track: evt.track
+      })
+    }
+    candidatePromise = waitForCandidate(pc)
     pushers.push(pusher)
+  } else {
+    pc = pusher.peerConnection
   }
-  const pc = new wrtc.RTCPeerConnection()
-  const candidatePromise = waitForCandidate(pc)
-  pusher.peerConnection = pc
-  pusher.tracks = []
-  pc.onsignalingstatechange = (evt)=>{
-    console.log(`#${pusher.id} onsignalingstatechange`, pc.signalingState)
-  }
-  pc.ontrack = (evt)=>{
-    const id = ++trackIdx
-    console.log(`#${pusher.id} ontrack ${id}`, evt.track.kind)
-    pusher.tracks.push({
-      id,
-      track: evt.track
-    })
-  }
-  const offer = new wrtc.RTCSessionDescription({
-    type: 'offer',
-    sdp: offerSdp
-  })
+  
+  const offer = new wrtc.RTCSessionDescription(jsep)
   pc.setRemoteDescription(offer)
   let answer = await pc.createAnswer()
   await pc.setLocalDescription(answer)
-  const candidates = await candidatePromise
+  let candidates = null
+  if (candidatePromise){
+    candidates = await candidatePromise
+  }
   res.json({
-    sdp: answer.sdp,
+    code: 200,
+    err_msg: "success",
+    trace_id: `${streamName}_${pusher.id}`,
+    jsep: answer,
     candidates: candidates
   })
 })
 
-app.post('/v1/edge/server/cid/:cid/pull/request', async (req, res) => {
-  const cid = req.params.cid
-  const uid = req.body.uid
-  const targetUid = req.body.dstUid
-  const offerSdp = req.body.sdp
-  console.log("PULL ", cid, targetUid, '=>', uid)
+app.post('/v1/rtc/play', async (req, res) => {
+  const streamName = req.body.stream_name
+  const update = req.body.update
+  const jsep = req.body.jsep
+  const uid = req.body.client_id
+  
+  console.log("PULL ", streamName, "update:", update, uid)
   let pusher = pushers.find((p)=>{
-    return p.cid === cid && p.uid === targetUid
+    return p.streamName === streamName
   })
   if (!pusher){
     res.json({
       code: 400,
-      errMsg: `没有发送端 cid: ${cid}, uid: ${targetUid}`
+      errMsg: `没有发送端 streamName: ${streamName}`
     })
     return;
   }else if (pusher?.peerConnection?.connectionState !== "connected"){
     res.json({
       code: 400,
-      errMsg: `发送端断了 cid: ${cid}, uid: ${targetUid}`
+      errMsg: `发送端断了 ${streamName}, ${pusher.id}`
     })
     return;
   }
-
-  let puller = pullers.find((p) => p.uid === uid);
-  if (puller){
+  let pullerIndex = uid ? pullers.findIndex((p) => p.uid === uid): -1;
+  let puller = pullers[pullerIndex];
+  if (puller && !update){
     puller.peerConnection.close()
-  }else{
-    pullerIdx++
-    puller = {id: pullerIdx}
+    pullers.splice(pullerIndex, 1)
+    puller = null
   }
-  const pc = new wrtc.RTCPeerConnection()
-  const candidatePromise = waitForCandidate(pc)
-  puller.peerConnection = pc
-
-  pusher.peerConnection.getReceivers().forEach((receiver)=>{
-    console.log("receiver", receiver, receiver.track)
-    console.log(`PULLER#${puller.id} PUSHER#${pusher.id} ${receiver.track.kind}`)
-    puller.peerConnection.addTrack(receiver.track)
-  })
+  let pc = null
+  let candidatePromise = null
+  if (!puller){
+    console.error("新的puller")
+    pc = new wrtc.RTCPeerConnection()
+    puller = {
+      streamName,
+      pusher,
+      id: ++pullerIdx,
+      peerConnection: pc,
+      tracks: [],
+    }
+    candidatePromise = waitForCandidate(pc)
+  }else{
+    console.log("已有的puller")
+    pc = puller.peerConnection
+  }
   
-  const offer = new wrtc.RTCSessionDescription({
-    type: 'offer',
-    sdp: offerSdp
+  pusher.tracks.forEach((trackInfo)=>{
+    // 多了多了
+    console.log("Add track", trackInfo.track.kind)
+    puller.peerConnection.addTrack(trackInfo.track)
   })
-  pc.setRemoteDescription(offer)
+  const offer = new wrtc.RTCSessionDescription(jsep)
+  await pc.setRemoteDescription(offer)
   let answer = await pc.createAnswer()
   await pc.setLocalDescription(answer)
   const candidates = await candidatePromise
   res.json({
-    sdp: answer.sdp,
+    trace_id: `${streamName}_${pusher.id}_${puller.id}`,
+    code: 200,
+    jsep: answer,
     candidates: candidates
   })
 })
 
-app.post('/proxy/edge/server/cid/:cid/:method/request', function (req, res){
-  const url = 'http://59.111.239.249:30443/v1/edge/server/cid/' + req.params.cid + "/" + req.params.method +"/request";
+app.post('/proxy/v1/rtc/:method', function (req, res){
+  const url = 'http://wecan-api.netease.im/v1/rtc/' + req.params.method;
   console.log("proxy", url)
   const body = req.body
   console.log("body", body)
@@ -160,6 +201,7 @@ const waitForCandidate = async (pc)=>{
   })
 }
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+const httpsServer = require("https").createServer(credentials, app)
+httpsServer.listen(port, () => {
+  console.log(`Example app listening at https://localhost.wrtc.dev:${port}/webrtc2.stateless.html`)
 })
