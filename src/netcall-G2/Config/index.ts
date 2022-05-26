f3bea6e08b338866862366f8263a8ce69b249ab0
scaret <scaret.in@gmail.com>
2022-05-26 10:08:21 +0800

    Bumped version 4.6.20

--- src/netcall-G2/Config/index.ts
+++ src/netcall-G2/Config/index.ts
@@ -1,6 +1,6 @@
 //Common Configs
-const SDK_VERSION = "4.6.10";// WEBPACK_STRING_REPLACE_VERSION -don't delete the comment
-const ENGINE_VERSION = "4.6.10.0";
+const SDK_VERSION = "4.6.20";// WEBPACK_STRING_REPLACE_VERSION -don't delete the comment
+const ENGINE_VERSION = "4.6.20.0";
 const BUILD = ""; // WEBPACK_STRING_REPLACE_BUILD -don't delete the comment
 
 export interface IConfig{
