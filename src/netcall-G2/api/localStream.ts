732d192c0c56fa1deb5d2499ffe84906730326cc
李海峰 <lihaifeng01@corp.netease.com>
2022-07-27 10:29:41 +0800

    基础美颜replacePluginTrack

--- src/netcall-G2/api/localStream.ts
+++ src/netcall-G2/api/localStream.ts
@@ -3500,7 +3500,8 @@ class LocalStream extends RTCEventEmitter {
             mediaType: "video",
             //@ts-ignore
             track: this._transformedTrack,
-            external: false
+            external: false,
+            noLowTrack: !isStart
       });
       //重新开启水印
       if (this.mediaHelper.video.preProcessingEnabled){
