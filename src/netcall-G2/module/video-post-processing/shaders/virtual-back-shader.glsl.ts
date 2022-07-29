efa5080f90ca102e35cdef1e185fbf8ca7c6f4af
杜亚龙 <duyalong@corp.netease.com>
2022-07-29 15:15:28 +0800

    fix: 背景分割替换黑帧

--- src/netcall-G2/module/video-post-processing/shaders/virtual-back-shader.glsl.ts
+++ src/netcall-G2/module/video-post-processing/shaders/virtual-back-shader.glsl.ts
@@ -13,28 +13,33 @@ export const virtualBackShader = {
     uniform vec2 size;
     uniform vec2 bkSize;
     uniform int backType;
+    uniform int emptyFrame;
 
     varying vec2 vuv;
     
     void main() {
-        vec3 color = texture2D(map, vuv).rgb;
-        float alpha = texture2D(maskMap, vuv).a;
+        if(emptyFrame > 0){
+            gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+        }else{
+            vec3 color = texture2D(map, vuv).rgb;
+            float alpha = texture2D(maskMap, vuv).a;
 
-        vec3 bk = backColor;
-        // 背景图
-        if(backType == 1){
-            float ratio = size.x / size.y;
-            float bkRatio = bkSize.x / bkSize.y;
-            vec2 suv = vuv;
-            if(ratio > bkRatio){
-                suv.y = (suv.y - 0.5) * bkRatio / ratio + 0.5;
-            }else{
-                suv.x = (suv.x - 0.5) * ratio / bkRatio + 0.5;
+            vec3 bk = backColor;
+            // 背景图
+            if(backType == 1){
+                float ratio = size.x / size.y;
+                float bkRatio = bkSize.x / bkSize.y;
+                vec2 suv = vuv;
+                if(ratio > bkRatio){
+                    suv.y = (suv.y - 0.5) * bkRatio / ratio + 0.5;
+                }else{
+                    suv.x = (suv.x - 0.5) * ratio / bkRatio + 0.5;
+                }
+                bk = texture2D(backMap, suv).rgb;
             }
-            bk = texture2D(backMap, suv).rgb;
-        }
 
-        gl_FragColor = vec4(mix(bk, color, alpha), 1.0);
+            gl_FragColor = vec4(mix(bk, color, alpha), 1.0);
+        }
     }
 `
 };
