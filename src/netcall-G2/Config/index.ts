2cdaee6d9e2156af6acbdad08b53ffc22eed7f3f
Easin Yao <easinyao@EasindeMacBook-Pro.local>
2022-08-17 16:10:25 +0800

    fix: change version

--- src/netcall-G2/Config/index.ts
+++ src/netcall-G2/Config/index.ts
@@ -1,6 +1,6 @@
 //Common Configs
-const SDK_VERSION = '4.6.21' // WEBPACK_STRING_REPLACE_VERSION -don't delete the comment
-const ENGINE_VERSION = '4.6.21.0'
+const SDK_VERSION = '4.6.25' // WEBPACK_STRING_REPLACE_VERSION -don't delete the comment
+const ENGINE_VERSION = '4.6.25.0'
 const BUILD = '' // WEBPACK_STRING_REPLACE_BUILD -don't delete the comment
 
 export interface IConfig {
