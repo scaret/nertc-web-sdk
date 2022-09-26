8a747c5c1af0a92e670b0835e68b963f470cb083
wulong <wulong@corp.netease.com>
2022-09-26 09:59:07 +0800

    fix:[修复私有化环境配置互动直播参数不可用的问题]

--- src/netcall-G2/module/meeting.ts
+++ src/netcall-G2/module/meeting.ts
@@ -616,6 +616,9 @@ class Meeting extends EventEmitter {
     if (this.adapterRef.instance._params.neRtcServerAddresses.roomServer) {
       //url = roomsTaskUrl.replace(/[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\.?/, this.adapterRef.instance._params.neRtcServerAddresses.roomServer)
       url = this.adapterRef.instance._params.neRtcServerAddresses.roomServer
+      if (url.substr(-1) !== '/') {
+        url += '/'
+      }
       this.logger.log('私有化配置的 roomsTaskUrl: ', url)
     }
 
@@ -766,6 +769,9 @@ class Meeting extends EventEmitter {
     if (this.adapterRef.instance._params.neRtcServerAddresses.roomServer) {
       //url = roomsTaskUrl.replace(/[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(\.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+\.?/, this.adapterRef.instance._params.neRtcServerAddresses.roomServer)
       url = this.adapterRef.instance._params.neRtcServerAddresses.roomServer
+      if (url.substr(-1) !== '/') {
+        url += '/'
+      }
       this.logger.log('私有化配置的 roomsTaskUrl: ', url)
     }
     url = `${url}${this.adapterRef.channelInfo.cid}/task/update`
