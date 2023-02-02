0b925b6e10a603f3a7bdc7c71a3c1caa0493b09a
scaret <scaret.in@gmail.com>
2023-02-02 11:29:51 +0800

    Bumped vesion 4.6.50

--- src/netcall-G2/Config/index.ts
+++ src/netcall-G2/Config/index.ts
@@ -1,6 +1,6 @@
 //Common Configs
-const SDK_VERSION = '4.6.40' // WEBPACK_STRING_REPLACE_VERSION -don't delete the comment
-const ENGINE_VERSION = '4.6.40.0'
+const SDK_VERSION = '4.6.50' // WEBPACK_STRING_REPLACE_VERSION -don't delete the comment
+const ENGINE_VERSION = '4.6.50.0'
 const BUILD = '' // WEBPACK_STRING_REPLACE_BUILD -don't delete the comment
 
 export interface IConfig {
