0a56f635b05cc0aaed047ac68020ee5a58687e36
scaret <scaret.in@gmail.com>
2023-02-22 15:13:56 +0800

    chore: 调整npm发布脚本，增加版本说明，npm发布过程仍为手动

--- build/npm/deploy-to-npm-repo.mjs
+++ build/npm/deploy-to-npm-repo.mjs
@@ -31,22 +31,25 @@ const wasmDir = path.join(pathNpmRepo, 'wasm');
 
 // 1. 验证SDK版本
 const commentLine = await $`head -n 1 ${sdkSrc}`;
-const match = commentLine.stdout.match(/NeRTC ([^ ]*)\|BUILD ([^ ]*) ([^ ]*)/);
+const firstLine = commentLine.stdout
+const match = firstLine.match(/NeRTC ([^ ]*)\|BUILD ([^ ]*) ([^ ]*)/);
 if (!match){
-  console.error("无法读取版本格式", commentLine.stdout)
+  console.error("\x1b[1;31m无法读取版本格式\x1b[m", commentLine.stdout)
   process.exit(1)
 }else if (VERSION !== match[1]){
-  console.error("版本信息不匹配", VERSION, match[1])
+  console.error("\x1b[1;31m版本信息不匹配\x1b[m", VERSION, match[1])
   process.exit(1)
 }else if (!match[2].match(/v\d+\.\d+\.\d+\-0-g[0-9a-f]+/)){
-  console.error("BUILD信息不对", match[2]);
-  process.exit(1)
+  console.error("\x1b[1;31mBUILD信息不对，但继续\x1b[m", match[2]);
 }else if (match[3] !== "production"){
-  console.error("环境错误：", match[3]);
+  console.error("\x1b[1;31m环境错误：\x1b[1;31m", match[3]);
   process.exit(1)
 }
 
-// 2. 拷贝SDK
+// 2. 准备git仓库
+await $`cd ${pathNpmRepo} && git reset --hard HEAD && git pull origin master && rm -rf *.js`
+
+// 3. 拷贝SDK
 await $`cp -f ${sdkSrc} ${sdkDest}`
 // 拷贝插件
 await $`cp -f ${VirtualBackgroundSrc} ${VirtualBackgroundDest}`
@@ -61,7 +64,7 @@ await $`cp -f ${AdvancedBeautyNoSimdWasmSrc} ${AdvancedBeautyNoSimdWasmDest}`
 await $`cp -f ${AIDenoiseSimdWasmSrc} ${AIDenoiseSimdWasmDest}`
 await $`cp -f ${AIDenoiseNoSimdWasmSrc} ${AIDenoiseNoSimdWasmDest}`
 
-// 3. 拷贝文档
+// 4. 拷贝文档
 await $`rm -rf ${path.join(pathNpmRepo, 'types')}`
 await $`cp -r ${path.join(__dirname, '../../docs/src/')} ${pathNpmRepo}/types`
 // 改变声明文件输出方式
@@ -69,10 +72,9 @@ let content = fs.readFileSync(`${pathNpmRepo}/types/nertc.d.ts`, 'utf-8');
 content = content.replace("export as namespace", "export =");
 fs.writeFileSync(`${pathNpmRepo}/types/nertc.d.ts`, content, 'utf-8');
 
-// 4. git操作
+// 5. git操作
+await $`cd ${pathNpmRepo} && npm version ${VERSION} --allow-same-version --no-git-tag-version`
 await $`cd ${pathNpmRepo} && git add -A .`
-await $`cd ${pathNpmRepo} && git commit -m "v${VERSION}"`
-
-// 5. 更新package.json
-await $`cd ${pathNpmRepo} && npm version ${VERSION}`
+await $`cd ${pathNpmRepo} && git commit -m "${firstLine.replace(/[^\w\-\.]/g, ' ').trim()}"`
+await $`cd ${pathNpmRepo} && git push origin master`
 
