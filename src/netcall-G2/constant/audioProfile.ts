a233075f8b7d9bbe705f6ce3e3e60e6845961282
scaret <scaret.in@gmail.com>
2023-05-09 16:54:40 +0800

    fix: 文件缺失

---
+++ src/netcall-G2/constant/audioProfile.ts
@@ -0,0 +1,16 @@
+export type AudioProfileTypes =
+  | 'speech_low_quality'
+  | 'speech_standard'
+  | 'music_standard'
+  | 'standard_stereo'
+  | 'high_quality'
+  | 'high_quality_stereo'
+
+export const AudioProfile: AudioProfileTypes[] = [
+  'speech_low_quality',
+  'speech_standard',
+  'music_standard',
+  'standard_stereo',
+  'high_quality',
+  'high_quality_stereo'
+]
