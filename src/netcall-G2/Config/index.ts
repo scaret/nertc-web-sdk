40880548cbc35a802ffd346a0d489a76efc23630
李海峰 <lihaifeng01@corp.netease.com>
2023-11-16 10:39:22 +0800

    修改版本号 5.5.11

--- src/netcall-G2/Config/index.ts
+++ src/netcall-G2/Config/index.ts
@@ -1,6 +1,6 @@
 //Common Configs
-const SDK_VERSION = '5.5.10' // WEBPACK_STRING_REPLACE_VERSION -don't delete the comment
-const ENGINE_VERSION = '5.5.10'
+const SDK_VERSION = '5.5.11' // WEBPACK_STRING_REPLACE_VERSION -don't delete the comment
+const ENGINE_VERSION = '5.5.11'
 const BUILD = '' // WEBPACK_STRING_REPLACE_BUILD -don't delete the comment
 
 export interface IConfig {
